5ce547713a6c6ffee92f3167d5f0adf7fdcf8a8c
gianlucasudano <sudanogianluca@gmail.com>
2021-05-03 19:44:23 +0200

    JSDoc and PropTypes on Item

--- src/components/DragAndSelect/elements/Item.js
+++ src/components/DragAndSelect/elements/Item.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import React, { useCallback, useEffect, useRef } from "react";
 import { StyledItem } from "./Item.styled";
 import { setFocusOnLongClickSelecting } from "../reducer/dragAndSelectActions";
@@ -9,11 +10,24 @@ import {
   onSelectingItems
 } from "./helpers";
 
+/**
+ * @param  {object} props - react props
+ * @param  {number} props.col - column number
+ * @param  {function} props.dispatch - dispatch from reducer
+ * @param  {boolean} props.isSelected - determine selected state
+ * @param  {array} props.items - all elements in the grid
+ * @param  {string} props.label - label to render on item
+ * @param  {number} props.mappingIndex - index from parent
+ * @param  {boolean} props.onSelecting - determine if the item is involved in a "long selection"
+ * @param  {number} props.row - row number
+ * @param  {number} props.selectingEventFirstItem - index of first selected item
+ * @param  {number} props.selectingEventLatestItem - index of latest selected item
+ * @param  {boolean} props.selectingEventStarted - determine if a "long selection" started
+ */
 const Item = ({
   col,
   dispatch,
   isSelected,
-  itemOrder,
   items,
   label,
   mappingIndex,
@@ -97,12 +111,26 @@ const Item = ({
       col={col}
       isSelected={isSelected}
       isSelecting={onSelecting}
-      itemOrder={itemOrder}
       row={row}
     >
       {label}
     </StyledItem>
   );
+};
+
+Item.propTypes = {
+  col: PropTypes.number,
+  dispatch: PropTypes.func,
+  isSelected: PropTypes.bool,
+  itemOrder: PropTypes.number,
+  items: PropTypes.array,
+  label: PropTypes.string,
+  mappingIndex: PropTypes.number,
+  onSelecting: PropTypes.bool,
+  row: PropTypes.number,
+  selectingEventFirstItem: PropTypes.number,
+  selectingEventLatestItem: PropTypes.number,
+  selectingEventStarted: PropTypes.bool
 };
 
 export default Item;
